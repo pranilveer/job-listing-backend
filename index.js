aedba741f4187d550cc0e3f75d35c024dbd67244
Pranil Dilip Veer <veerpranil@gmail.com>
2023-12-09 13:00:57 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
-// const mongoose = require('mogoose');
+const mongoose = require('mongoose');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,4 +15,24 @@ app.get('/', (req, res) => {
     res.send('Hello from server')
 })
 
-module.exports = app;
+app.get("/health", (req, res) => {
+    const dbStatus = mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+  
+    res.json({
+      server: "Running",
+      database: dbStatus,
+    });
+  });
+
+app.listen(port, () =>{
+    mongoose
+    .connect(process.env.DB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected");
+      console.log(`App listening at http://localhost:${process.env.port}`);
+    })
+    .catch((err) => console.log(err));
+})
