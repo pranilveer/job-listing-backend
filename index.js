561577e528c26080497aa345b66a5b5c08e7335a
Pranil Dilip Veer <veerpranil@gmail.com>
2023-12-15 16:32:59 +0530

    added error handler middleware

--- index.js
+++ index.js
@@ -3,16 +3,102 @@ const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
+
+const User = require("./models/userModel");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 dotenv.config();
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, mobileNumber, password } = req.body;
+
+        // Check if the required fields are provided
+        if (!name || !email || !mobileNumber || !password) {
+            return res
+                .status(400)
+                .json({ error: "Please provide all required fields" });
+        }
+
+        // Check if a user with the same email already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ error: "User already exists" });
+        }
+
+        // Hash the password using bcrypt
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Create a new user
+        const newUser = new User({
+            name,
+            email,
+            mobileNumber,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// User login
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if the required fields are provided
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ error: "Please provide email and password" });
+        }
+
+        // Find the user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        // Compare the provided password with the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        // Generate and return the JWT token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to login" });
+    }
+});
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res
+        .status(500)
+        .json({ error: "Something went wrong! Please try again later." });
+});
 
 app.get("/health", (req, res) => {
     const dbStatus = mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
